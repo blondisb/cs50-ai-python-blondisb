e9f2f7e5a5539f433e1e06f9e3bb43ca1b54ffb7
blondisb <blondisb@uninorte.edu.co>
2023-12-09 02:02:31 -0500

    Start project00b

---
+++ C0_search/Project00/tictactoe/test.py
@@ -0,0 +1,8 @@
+
+a = not(True)
+print(a)
+
+c = 1
+matrix = [[5, 2], [3, 4]]
+b = not(any(c in row for row in matrix))
+print(b)
---
+++ C0_search/Project00/tictactoe/tictactoe.py
@@ -0,0 +1,82 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    print('player', board)
+    count = 0
+    for row in board:
+        for element in row:
+            if element == None:
+                count += 1
+                print('player', count)
+    
+    if count == 9:
+        return X
+    elif count % 2 == 0:
+        return O
+    else:
+        return X
+
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    raise NotImplementedError
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    raise NotImplementedError
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    raise NotImplementedError
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    # If I found None, then Any=True, but I need false. So, I use not().
+    return not(any(None in row for row in board))
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    raise NotImplementedError
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    raise NotImplementedError
